716af6938ec133597987368bc0ec7e7750ef2483
TARIUS S.A.S. <Hernan.Jurado@tarius.ec>
2026-03-30 20:54:06 -0500

    Chek Point

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,24 +1,2 @@
-// Archivo: test/widget_test.dart
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:tarius_valora/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Construye la app y dibuja el primer frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verifica que el contador empieza en 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Simula un tap en el ícono '+' y dibuja el siguiente frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verifica que el contador ahora muestra 1.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
 }
